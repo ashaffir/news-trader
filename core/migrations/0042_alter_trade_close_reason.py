c97c4d4e4d9d94d7be45684126d0104145618e3c
alshaffir <alfredsh@etoro.com>
2025-09-26 12:43:34 +0300

    migration issues 2

---
+++ core/migrations/0042_alter_trade_close_reason.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0041_add_duplicate_sync_reason"),
+    ]
+
+    operations = []
+
+
---
+++ core/migrations/0044_merge_0042_0043.py
@@ -0,0 +1,13 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0042_alter_trade_close_reason"),
+        ("core", "0043_add_llm_calculation_params"),
+    ]
+
+    operations = []
+
+
