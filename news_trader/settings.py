857f68be4f6b75cab2b6b18e4ceae471548ddbe6
ashaffir <alfred.shaffir@gmail.com>
2025-10-21 12:16:36 +0300

    csrf settings

--- news_trader/settings.py
+++ news_trader/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-me-in-production')
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 'yes', 'on')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0,web').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 INSTALLED_APPS = [
